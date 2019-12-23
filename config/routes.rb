275794b0f42b098ee6fb85f73e5630adb266769a
Venkat Ramaraju <venky2063@gmail.com>
2019-12-23 11:30:02 +0530

    To do, doing and done features implemented with links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :tasks do
+  	member do
+  		put :change
+  	end
+  end
+  
   root 'pages#home'
   resources :tasks
   get 'about' => 'pages#about'
