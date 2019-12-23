dc49f28a0923fa96b44d038faf5d2fd7c4e2af85
Venkat Ramaraju <venky2063@gmail.com>
2019-12-23 10:07:55 +0530

    Added authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_014019) do
+ActiveRecord::Schema.define(version: 2019_12_23_042119) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
